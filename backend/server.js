53b0072db493372cd886a70e7b04dc281db31b6c
taukirsheikh <taukir@portpro.io>
2024-08-06 21:33:00 +0545

    formatted

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const colors = require("colors");
 
-app.get('/', function (req, res) {
-    // res.json('Hello World!'); // works
-    res.json({message: 'Hello World!'});
-})
+app.get("/", function (req, res) {
+  // res.json('Hello World!'); // works
+  res.json({ message: "Hello World!" });
+});
 
 //listen for request
-app.listen(4000, function portRunner() {
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, function portRunner() {
   console.log(
-    "server is running on port 4000".inverse.green,
+    "server is running on port".green + PORT.inverse.green,
     this.address().port.toString().red
   );
 });
